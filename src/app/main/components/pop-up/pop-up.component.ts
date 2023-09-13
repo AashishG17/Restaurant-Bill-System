42af57e57c3c6435c5c08a86780b2363ce7599c7
Aashish Magar <aashish1.magar@ekbana.info>
2023-09-13 21:47:47 +0545

    feat: add button functionalities.

--- src/app/main/components/pop-up/pop-up.component.ts
+++ src/app/main/components/pop-up/pop-up.component.ts
@@ -1,20 +1,37 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+
+import { IItem, IMenu } from 'src/app/core/interfaces/item.interface';
+import { addOrder } from 'src/app/core/store/item.actions';
 
 @Component({
   selector: 'app-pop-up',
   templateUrl: './pop-up.component.html',
   styleUrls: ['./pop-up.component.scss']
 })
-export class PopUpComponent implements OnInit{
+export class PopUpComponent implements OnInit, OnDestroy {
 
   constructor(
+    private store: Store<{ menu: IMenu }>,
     public dialogRef: MatDialogRef<PopUpComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(MAT_DIALOG_DATA) public data: { item: IItem },
   ) {}
 
   ngOnInit(): void {
-    console.log(this.data)
+    // console.log(this.data)
   }
 
+  onSave(): void {
+    this.store.dispatch(addOrder({ orders: this.data.item }));
+    this.dialogRef.close();
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnDestroy(): void {
+    this.dialogRef?.close();
+  }
 }
