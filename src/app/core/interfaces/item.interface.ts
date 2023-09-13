76bd4ad7e15ca57166c7df1e2a9c4420394a6c91
Aashish Magar <aashish1.magar@ekbana.info>
2023-09-13 21:41:04 +0545

    Add interface.

--- src/app/core/interfaces/item.interface.ts
+++ src/app/core/interfaces/item.interface.ts
@@ -1,7 +1,19 @@
-export interface ItemInterface {
+export interface IItem {
     category: string;
+    categoryId: number;
     id: number;
     name: string;
     price: number;
     thumbnail: string;
+    quantity?: number;
+}
+
+export interface ICategory {
+    id: number;
+    name: string;
+}
+
+export interface IMenu {
+    items: IItem[],
+    orders: IItem[],
 }
