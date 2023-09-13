3f7000e4a68213e5c0fbba6e42c374dd580510b1
Aashish Magar <aashish1.magar@ekbana.info>
2023-09-13 21:40:41 +0545

    Add modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 import { MainModule } from 'src/app/main/main.module';
+import { ItemEffect } from 'src/app/core/store/item.effects';
+import { menuReducer } from 'src/app/core/store/item.reducers';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,10 @@ import { MainModule } from 'src/app/main/main.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MainModule,
+    StoreModule.forRoot({ menu: menuReducer }),
+    EffectsModule.forRoot([ItemEffect]),
   ],
   providers: [],
   bootstrap: [AppComponent],
