0fd970887f1af9ecc6fd6fedb5535b33ccf5670c
Aashish Magar <aashish1.magar@ekbana.info>
2023-09-13 21:45:48 +0545

    feat: get data from store, add/change categories

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 import { PopUpComponent } from 'src/app/main/components/pop-up/pop-up.component';
-import { ItemInterface } from 'src/app/core/interfaces/item.interface';
+import { ICategory, IItem, IMenu } from 'src/app/core/interfaces/item.interface';
 import { ItemService } from 'src/app/core/services/item.service';
+import { getItems, getOrders } from 'src/app/core/store/item.selectors';
 
 @Component({
   selector: 'app-main',
@@ -13,27 +16,78 @@ import { ItemService } from 'src/app/core/services/item.service';
 })
 export class MainComponent implements OnInit, OnDestroy{
   popupDialog!: MatDialogRef<PopUpComponent>;
-  items!: ItemInterface[];
-  cartItems = [1,2,3,4,5,6,7,8,9]
+  subscription: Subscription[] = [];
+  items!: IItem[];
+  orders!: IItem[];
+  categories!: ICategory[];
 
   constructor(
+    private readonly store: Store<{ menu: IMenu }>,
     private readonly dialog: MatDialog,
     private readonly itemService: ItemService,
   ) {}
 
   ngOnInit(): void {
-    this.items = this.itemService.items;
+    this.getDataFromStore();
+    this.getItems();
+    this.getCategories();
+  }
+
+  getDataFromStore(): void {
+    this.subscription.push(
+      this.store.select(getItems).subscribe({
+        next: (data) => {
+          console.log('from item', data)
+        }
+      }),
+      this.store.select(getOrders).subscribe({
+        next: (data) => {
+          this.orders = data;
+        }
+      }),
+    );
+  }
+
+  getItems(): void {
+    this.subscription.push(
+      this.itemService.getItems().subscribe({
+        next: (response: IItem[]) => {
+          this.items = response;
+        }
+      }),
+    )
+  }
+
+  getCategories(): void {
+    this.subscription.push(
+      this.itemService.getCategories()
+        .subscribe({
+          next: (response: ICategory[]) => {
+            this.categories = response;
+          }
+        }),
+    )
+  }
+
+  changeCategory(category?: string): void {
+    const items = this.items.filter((item: IItem) => {
+      if (item.category === category) {
+        return item;
+      }
+      return this.items;
+    });
+    this.items = items;
   }
 
   openPopup(item: any): void {
     this.popupDialog = this.dialog.open(PopUpComponent, {
-      data: { name: 'hello', age: 12 }
+      data: { item }
     });
-    console.log(item)
   }
 
   ngOnDestroy(): void {
     this.popupDialog.close();
+    this.subscription.forEach(subscription => subscription.unsubscribe());
   }
 
 }
