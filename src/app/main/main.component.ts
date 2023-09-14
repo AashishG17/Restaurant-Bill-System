8dbbf12d17eb61b48d8049272466176ba96f4db0
Aashish Magar <aashish1.magar@ekbana.info>
2023-09-14 15:51:33 +0545

    feat: add change category functionalities.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
@@ -7,8 +7,7 @@ import { PopUpComponent } from 'src/app/main/components/pop-up/pop-up.component'
 import { ICategory, IItem, IMenu } from 'src/app/core/interfaces/item.interface';
 import { ItemService } from 'src/app/core/services/item.service';
 import { getItems, getOrders } from 'src/app/core/store/item.selectors';
-import { decOrder, delOrder, incOrder } from 'src/app/core/store/item.actions';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { decOrder, delOrder, incOrder, loadItems } from 'src/app/core/store/item.actions';
 
 @Component({
   selector: 'app-main',
@@ -19,23 +18,39 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class MainComponent implements OnInit, OnDestroy{
   popupDialog!: MatDialogRef<PopUpComponent>;
   subscription: Subscription[] = [];
-  items!: IItem[];
+  private items!: IItem[];
+  displayedItems!: IItem[];
   orders!: IItem[];
   categories!: ICategory[];
+  selectedCategory!: string;
   displayedColumns: string[] = ['name', 'price', 'quantity', 'subtotal', 'action'];
+  menuListColSpan:number = 5;
+  menuItemColSpan:number = 7;
   currentPage: number = 1;
   itemsPerPage: number = 9;
+  totalPage: number = 1;
+
+  @HostListener('window:resize') windowResize() {
+    let windowWidth = window.innerWidth;
+
+    if(windowWidth<850){
+      this.menuListColSpan = 12;
+      this.menuItemColSpan = 12;
+    }else{
+      this.menuListColSpan = 5;
+      this.menuItemColSpan = 7;
+    }
+  }
 
   constructor(
     private readonly store: Store<{ menu: IMenu }>,
     private readonly dialog: MatDialog,
     private readonly itemService: ItemService,
-    private readonly snackBar: MatSnackBar,
   ) {}
 
   ngOnInit(): void {
+    this.store.dispatch(loadItems());
     this.getDataFromStore();
-    this.getItems();
     this.getCategories();
   }
 
@@ -43,7 +58,10 @@ export class MainComponent implements OnInit, OnDestroy{
     this.subscription.push(
       this.store.select(getItems).subscribe({
         next: (data) => {
-          console.log('from item', data)
+          this.items = data;
+          this.displayedItems = data;
+          this.selectedCategory = 'all';
+          this.totalPages();
         }
       }),
       this.store.select(getOrders).subscribe({
@@ -57,17 +75,7 @@ export class MainComponent implements OnInit, OnDestroy{
   get displayedData() {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
-    return this.items.slice(startIndex, endIndex);
-  }
-
-  getItems(): void {
-    this.subscription.push(
-      this.itemService.getItems().subscribe({
-        next: (response: IItem[]) => {
-          this.items = response;
-        }
-      }),
-    )
+    return this.displayedItems?.slice(startIndex, endIndex);
   }
 
   getCategories(): void {
@@ -82,13 +90,21 @@ export class MainComponent implements OnInit, OnDestroy{
   }
 
   changeCategory(category?: string): void {
-    const items = this.items.filter((item: IItem) => {
-      if (item.category === category) {
-        return item;
-      }
-      return this.items;
-    });
-    this.items = items;
+    if (category) {
+      const items = this.items.filter((item: IItem) => {
+        if (item.category === category) {
+          return item;
+        }
+        return;
+      });
+      this.selectedCategory = category;
+      this.displayedItems = items;
+    } else {
+      this.displayedItems = this.items;
+      this.selectedCategory = 'all';
+    }
+    this.currentPage = 1;
+    this.totalPages();
   }
 
   increaseQuantity(id: number, qty?: number): void {
@@ -101,15 +117,7 @@ export class MainComponent implements OnInit, OnDestroy{
 
   deleteOrder(id: number): void {
     this.store.dispatch(delOrder({ id: id }));
-    this.showSnackBar('Order Removed!');
-  }
-
-  showSnackBar(msg: string): void {
-    this.snackBar.open(msg, 'Dismiss', {
-      duration: 2000,
-      horizontalPosition: 'end',
-      verticalPosition: 'top',
-    });
+    this.itemService.showSnackBar('Order Removed!');
   }
 
   prevPage(): void {
@@ -125,7 +133,9 @@ export class MainComponent implements OnInit, OnDestroy{
   }
 
   totalPages(): number {
-    return Math.ceil(this.items.length / this.itemsPerPage);
+    const total = Math.ceil(this.displayedItems.length / this.itemsPerPage);
+    this.totalPage = total;
+    return total;
   }
 
   getPaginationArray(): number[] {
